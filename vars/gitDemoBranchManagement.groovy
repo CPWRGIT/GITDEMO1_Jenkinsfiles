2910916d6c1457d33d427c6a2e8f8bdeb32e8444
CWDE-RNUESSE <ralph.nuesse@compuware.com>
2020-12-15 09:30:45 +0100

    BM delete

--- vars/gitDemoBranchManagement.groovy
+++ vars/gitDemoBranchManagement.groovy
@@ -1,16 +1,73 @@
-def ispwFtLevelRepl = "." + IspwTargetLevel + "."
-def ispwUtLevelRepl = ".UT" + IspwTargetLevel.substring(2, 3) + "."
-
-def gitRepo         = "https://github.com/CPWRGIT/${HostUserId}.git"
-def newBranchName   = 'feature/' + IspwTargetLevel + '/' + branchName
+def localBranchName
 def consoleMessage
-
-HostUserId          = HostUserId.toUpperCase()
+def ispwFtLevelRepl
+def ispwUtLevelRepl
+def gitRepo
 
 node {
 
-    dir("./"){
-        deleteDir()
+    stage("Initialize"){
+
+        ispwFtLevelRepl = "." + IspwTargetLevel + "."
+        ispwUtLevelRepl = ".UT" + IspwTargetLevel.substring(2, 3) + "."
+
+        HostUserId          = HostUserId.toUpperCase()
+
+        if (HostUserId == ''){
+
+            error "[ERROR] - The Host User Id needs to be specified. Aborting execution."
+
+        }
+
+        if(BranchName = ''){
+
+            error "[ERROR] - The Branch Name needs to be specified. Aborting execution."
+
+        }
+
+        if(GitHubUserName == ''){
+
+            error "[ERROR] - The GitHub User Name needs to be specified. Aborting execution."
+
+        }
+
+        if(GitHubPassword == ''){
+
+            error "[ERROR] - The GitHub Password needs to be specified. Aborting execution."
+
+        }
+
+        if(BranchAction == "Create"){
+
+            if(BranchType == "Feature"){
+                
+                if(!(IspwTargetLevel.contains("FT"))){
+                    error "[ERROR] - The Branch Name needs to be specified. Aborting execution."
+                }
+                else
+                {
+                    localBranchName = 'feature/' + IspwTargetLevel + '/' + branchName
+                }
+            }
+            else{
+                localBranchName = 'bugfix'
+            }
+        }
+        else{
+
+            if(!(BranchName.contains('feature/FT'))){
+
+                error "[ERROR] - For a DELETE Action you need to specify the full Branch Name."
+
+            }
+
+        }
+
+        def gitRepo         = "https://github.com/CPWRGIT/${HostUserId}.git"
+
+        dir("./"){
+            deleteDir()
+        }
     }
 
     stage("Checkout"){
@@ -22,38 +79,39 @@ node {
                 $class: 'GitSCM', 
                 branches: [[name: '*/development']], 
                 doGenerateSubmoduleConfigurations: false, 
-                extensions: [[$class: 'LocalBranch', localBranch: newBranchName]],
+                extensions: [[$class: 'LocalBranch', localBranch: localBranchName]],
                 submoduleCfg: [], 
                 userRemoteConfigs: [[url: gitRepo]]
             ]
         )
     }
     
-    def contextFileList = findFiles(glob: '**/Tests/**/*.context')
-    
     stage("Modify .context files"){
 
         if(BranchAction == 'Create'){
 
+            def contextFileList = findFiles(glob: '**/Tests/**/*.context')
+
             if (BranchType == "Feature")
             {
 
-                newBranchName = 'feature/' + IspwTargetLevel + '/' + branchName
+                localBranchName = 'feature/' + IspwTargetLevel + '/' + branchName
 
             }
             else{
 
-                newBranchName = 'bugfix'
+                localBranchName = 'bugfix'
 
             }
 
-            echo "Branch: " + newBranchName
+            echo "[INFO] - Modifying .context files for branch " + localBranchName + "."
+            echo "[INFO] - Setting load library UT and FT level qualifiers to " + ispwUtLevelRepl + " and " + ispwFtLevelRepl + "."
 
             contextFileList.each {
                 
                 if (!(it.name.contains('Jenkins'))){
                     
-                    echo "Modifying: " + it.path
+                    echo "[Info] - Modifying file: " + it.path
                     
                     def contextFileContent  = readFile(file: it.path)
                     contextFileContent      = contextFileContent.replace(".FT1.", ispwFtLevelRepl).replace(".UT1.", ispwUtLevelRepl).replace(".FT2.", ispwFtLevelRepl).replace(".UT2.", ispwUtLevelRepl).replace(".FT3.", ispwFtLevelRepl).replace(".UT3.", ispwUtLevelRepl).replace(".FT4.", ispwFtLevelRepl).replace(".UT4.", ispwUtLevelRepl)
@@ -64,22 +122,44 @@ node {
         }
         else{
 
-            echo "delete not implemented yet"
+            echo "Deleting branch. No files to modify."
 
         }
     }
 
     stage("Branch Action"){
 
-        dir("./")
-        {
-            def message = '"Inital Setup new Branch"'
-            consoleMessage = bat(returnStdout: true, script: 'git status')
-            echo consoleMessage
-            consoleMessage = bat(returnStdout: true, script: 'git commit -a -m ' + message)
-            echo consoleMessage
-            consoleMessage = bat(returnStdout: true, script: "git push  https://${GitHubUserName}:${GitHubPassword}@github.com/CPWRGIT/${HostUserId} refs/heads/${newBranchName}:refs/heads/${newBranchName} -f")
-            echo consoleMessage
+        if(BranchAction == 'Create'){
+
+            dir("./")
+            {
+                echo "[INFO] - Creating branch " + localBranchName + "on remote repository."
+
+                def message     = '"Inital Setup for Branch ' + localBranchName + '"'
+                consoleMessage  = bat(returnStdout: true, script: 'git status')
+                echo consoleMessage
+                consoleMessage  = bat(returnStdout: true, script: 'git commit -a -m ' + message)
+                echo consoleMessage
+                consoleMessage  = bat(returnStdout: true, script: "git push  https://${GitHubUserName}:${GitHubPassword}@github.com/CPWRGIT/${HostUserId} refs/heads/${localBranchName}:refs/heads/${localBranchName} -f")
+                echo consoleMessage
+            }
+        }
+        else{
+
+            dir("./")
+            {
+                echo "[INFO] - Creating branch " + localBranchName + "on remote repository."
+
+                def message     = '"Delete Branch ' + localBranchName + '"'
+                consoleMessage  = bat(returnStdout: true, script: "git branch -d ${localBranchName}")
+                echo consoleMessage
+                consoleMessage  = bat(returnStdout: true, script: 'git commit -a -m ' + message)
+                echo consoleMessage
+                consoleMessage  = bat(returnStdout: true, script: "git push  https://${GitHubUserName}:${GitHubPassword}@github.com/CPWRGIT/${HostUserId} --delete refs/heads/${localBranchName} -f")
+                echo consoleMessage
+            }
+
+
         }
 
     }
