c4e2fd4749d31ed3f5a9cfdd3a4271544ba32f6f
CWDE-RNUESSE <ralph.nuesse@compuware.com>
2020-12-21 14:54:34 +0100

    Tag Release Script

---
+++ vars/gitDemoTagRelease.groovy
@@ -0,0 +1,66 @@
+def tagName = "v${ReleaseNumber}"
+
+currentBuild.displayName = "Tagging ${IspwApplication} - ${tagName}"
+
+node{
+    
+    dir("./"){
+        
+        deleteDir()
+        
+    }
+    
+    stage("Clone Repo"){
+        
+        checkout(
+            changelog: false, 
+            poll: false, 
+            scm: [
+                $class: 'GitSCM', 
+                branches: [[name: '*/main']], 
+                doGenerateSubmoduleConfigurations: false, 
+                extensions: [], 
+                submoduleCfg: [], 
+                userRemoteConfigs: [
+                    [
+                        credentialsId: GitCredentials, 
+                        url: "https://github.com/CPWRGIT/${GitRepo}.git"
+                    ]
+                ]
+            ]
+        )
+    }
+    
+    stage("Add Tag"){
+        
+        def gitMessage = '"Release ' + tagName + '"'
+        
+        def stdOut = bat(
+            label: '', 
+            returnStdout: true, 
+            script: 'git tag -a ' + tagName + ' -m ' + gitMessage
+        )
+        
+        echo stdOut
+        
+        withCredentials(
+            [
+                usernamePassword(
+                    credentialsId:      GitCredentials, 
+                    passwordVariable:   'GitHubPassword', 
+                    usernameVariable:   'GitHubUser'
+                )
+            ]
+        ) 
+        {
+
+            stdOut = bat(
+                returnStdout: true, 
+                script: "git push origin ${tagName}"
+            )
+    
+            echo stdOut
+            
+        }
+    }
+}
