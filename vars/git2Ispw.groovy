e875edf9d51a8604ba57f1a94f47d3316d5c8628
CWDE-RNUESSE <ralph.nuesse@compuware.com>
2020-12-22 08:46:13 +0100

    Add XLR

--- vars/git2Ispw.groovy
+++ vars/git2Ispw.groovy
@@ -146,6 +146,76 @@ def call(Map execParms){
             runSonarScan()
 
         }   
+
+        if(BRANCH_NAME == 'main') {
+
+            def cesToken 
+            def ownerId
+
+            withCredentials(
+                [
+                    string(
+                        credentialsId:  pipelineParms.cesCredentialsId, 
+                        variable:       'cesTokenTemp'
+                    )
+                ]
+            ) 
+            {
+
+                cesToken = cesTokenTemp
+
+            }
+
+            withCredentials(
+                [
+                    string(
+                        credentialsId:  pipelineParms.hostCredentialsId, 
+                        variable:       'ownerIdTemp'
+                    )
+                ]
+            ) 
+            {
+
+                ownerId = ownerIdTemp
+
+            }
+
+            def assignmentId = getMainAssignmentId()
+
+            xlrCreateRelease(
+                releaseTitle:       "GITDEMO - Release for ${ispwConfig.ispwApplication.application}", 
+                serverCredentials:  'admin', 
+                startRelease:       true, 
+                template:           'GITDEMO_CWCC', 
+                variables: [
+                    [
+                        propertyName:   'ISPW_Application', 
+                        propertyValue:  ispwConfig.ispwApplication.application
+                    ], 
+                    [
+                        propertyName:   'ISPW_Assignment', 
+                        propertyValue:  assignmentId
+                    ], 
+                    [
+                        propertyName:   'Owner_Id', 
+                        propertyValue:  ownerId
+                    ],
+                    [
+                        propertyName:   'CES_Token', 
+                        propertyValue:  cesToken
+                    ], 
+                    [
+                        propertyName: 'Jenkins_CES_Credentials', 
+                        propertyValue: pipelineParms.cesCredentialsId
+                    ],
+                    [
+                        propertyName: 'Jenkins_Git_Credentials', 
+                        propertyValue: pipelineParms.gitCredentialsId
+                    ] 
+                ]
+            )
+
+        }
     }
 }
 
@@ -614,4 +684,12 @@ def getSonarResults(resultsFileList){
     }
 
     return resultsList
+}
+
+def getMainAssignmentId(){
+
+    def automaticBuildFileContent = readJSON(file: automaticBuildFile)
+
+    return automaticBuildFileContent.containerId
+    
 }
