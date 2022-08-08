d3a6f6eab77a37e41b6d6598685f0d73fcc47ca2
Ralph Nuesse <ralph_nuesse@bmc.com>
2022-08-08 09:48:03 +0200

    Gen Parms

--- vars/mfCodePipeline.groovy
+++ vars/mfCodePipeline.groovy
@@ -355,7 +355,8 @@ def prepMainframeBuild(){
         url:            "http://cwcc.compuware.com:2020/ispw/ispw/assignments/" + automaticBuildInfo.containerId + "/tasks/" + automaticBuildInfo.taskIds[0],
         contentType:    "APPLCIATION_JSON",
         authentication: "665fc9fb-39de-428a-8a67-a3619752873d"
-
+    )
+    
     echo "RESPONSE: " + response.toString()
 
 }
